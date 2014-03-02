ced054a83a4f450408a9e00a6c2e6a2a2c6622bc
vijayrajah <me@rvijay.in>
2014-03-02 23:29:59 +0529

    Initial Commit

---
+++ swap-usage/swap-usage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+##Check with pid's are using swap
+
+#IFS=" "
+
+for i in `ls -d /proc/[1-9]*`
+do
+	#echo $i
+	PSUM=0
+	PSUM=`grep Swap ${i}/smaps 2>/dev/null | awk '{SUM+=$2} END {print SUM}'`
+	if [ ! -z $PSUM ]; then
+
+		if (( $PSUM > 0)); then
+			PID=`basename $i`
+			COMM=`ps -p $PID -o comm --no-headers`
+			echo "$PID: $COMM : $PSUM"
+		fi
+	fi
+
+done
